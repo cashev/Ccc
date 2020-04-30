07faef4dccef262c2bfa72c96bce20263704d0be
cashew <42924021+cashev@users.noreply.github.com>
2020-04-30 23:00:51 +0900

    offsetについて修正
    変数領域が足りていなかったため修正

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
   // 変数26個分の領域を確保する
   printf("  push rbp\n");
   printf("  mov rbp, rsp\n");
-  printf("  sub rsp, %d\n", offset);
+  printf("  sub rsp, %d\n", offset + 8);
 
   // 先頭の式から順にコード生成
   for (int i = 0; code[i]; i++) {
